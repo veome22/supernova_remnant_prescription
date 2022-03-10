df1029f296b3fc2cecc47877297b048475c5ddb4
Veome Kapil <veomekapil@gmail.com>
2022-03-10 15:19:55 -0600

    Cleaned up the likelihood code, added better print statements

--- natal_kick_tools/mandel_muller_likelihood_functions.py
+++ natal_kick_tools/mandel_muller_likelihood_functions.py
@@ -107,12 +107,16 @@ def p_vi_from_model(vt, model):
 
 def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigmas=[0.3], local=False):
     '''
-    This function takes in a specific Muller Mandel prescription and calculates the probability of drawing a set of pulsars from it.
+    This function takes in a range of Muller Mandel prescriptions and calculates the probability of drawing a given set of pulsars from them.
+    
     Arguments:
     pulsar_data_loc:    The folder which contains the pulsar posterior data
-    bh_kick:            v_bh multiplier used to generate kicks from the reference model
-    ns_kick:            v_ns multiplier used to generate kicks from the reference model
-    sigma:              sigma used to generate kicks from the reference model
+    bh_kicks:           List of v_bh multipliers used to generate kicks (from the reference model)
+    ns_kicks:           List of v_ns multipliers used to generate kicks (from the reference model)
+    sigmas:             List of sigmas used to generate kicks (from the reference model)
+    local:              a flag to switch to local versions of data loading functions. Should be replaced with a model_data_loc 
+                        parameter in the future
+                        
     '''
     
     # Read in the model kicks 
@@ -125,17 +129,18 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
     vt_all = []
     for file in glob.glob(f'{pulsar_data_loc}/*.bootstraps'):
         vt_all.append(np.loadtxt(file, unpack=True, usecols=5))
-    print(f"Successfully read {len(vt_all)} data files")
+    print(f"\n Successfully read {len(vt_all)} pulsar data files\n")
 
+    likelihoods = np.empty(len(SN_KICKS_NS)) # array to store combined likelihoods for all models
+    
     for k in range(len(SN_KICKS_NS)):       
         model_data = SN_KICKS_NS[k]
 
-
-        start = time.time()       
+        start = time.time() 
+        
         # Create interpolated pdf from model kicks
         y,x = np.histogram(model_data, density=True, bins="scott")
         model_pdf = interpolate.interp1d(x[:-1], y, bounds_error=False, fill_value=0)
-
 
         # Create array of likelihoods for all the pulsar objects
         p_vi_M_all = np.empty(len(vt_all), dtype=object)
@@ -146,24 +151,22 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
         for i in range(len(vt_all)):
             p_vi_M = model_pdf(vt_all[i]) # calculate probability of drawing each velocity in the given pulsar posterior         
             p_vi_M_all[i] = p_vi_M
-            p_di_M[i] = np.average(p_vi_M_all[i])
+            p_di_M[i] = np.mean(p_vi_M_all[i])
 
         end = time.time()
         print(f"Likelihood calculation for vns_{NS_KICK_MULT[k]}_sigma_{SIGMAS[k]} completed in:", end - start, "s")
 
-
-
-        #     for i in range(len(p_vi_M_all)):
-        #         p_di_M[i] = np.average(p_vi_M_all[i])
-        
 
         # Save probabilities of drawing each pulsar given the model
         fname = f"../calculatedModelLikelihoods/vns_{NS_KICK_MULT[k]}_sigma_{SIGMAS[k]}"
         print(f"Writing pulsar probabilities to file: {fname}")
         np.savetxt(fname, p_di_M)
 
+        likelihoods[k] = np.prod(p_di_M)
         print("Calculation Complete!")
-    return np.prod(p_di_M)
+        print()
+        
+    return likelihoods
 
 
 
