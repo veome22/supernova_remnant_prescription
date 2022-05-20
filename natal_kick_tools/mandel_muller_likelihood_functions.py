db73608b551f5a40de34aaf1ed6cc1452be0ed72
Veome Kapil <veomekapil@gmail.com>
2022-05-20 13:23:30 -0500

    changed all folder-specific references to options, added code to load bse simulations

--- natal_kick_tools/mandel_muller_likelihood_functions.py
+++ natal_kick_tools/mandel_muller_likelihood_functions.py
@@ -5,25 +5,32 @@ import glob
 import time
 import os
 
-def load_sim_data(bh_kicks=[200], ns_kicks=[300, 400, 700], sigmas = [0.1, 0.3, 0.5], \
-                  workdir = os.environ['WORK'] + f'/supernova_remnant_production'):  
+def load_sim_data(bh_kicks=[200], ns_kicks=[300, 400, 700], sigmas = [0.1, 0.3, 0.5], mode='sse', \
+                  work_dir = os.environ['WORK'] + f'/supernova_remnant_production'):  
                
     SN_KICK_BH_ALL = []
     SN_KICK_NS_ALL = []
     NS_KICK_MULT = []
     SIGMAS = []
        
+
     for bh_kick in bh_kicks:
         for ns_kick in ns_kicks:
             for sigma in sigmas:
-                path = workdir + f'/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
+                path = work_dir + f'/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
                 print("Loading Mandel Muller model data from", path)
             
                 fdata = h5.File(path, 'r')
-
-                SN_STELLAR_TYPE = fdata['SSE_Supernovae']["Stellar_Type"][...].squeeze()
-                SN_TYPE = fdata['SSE_Supernovae']["SN_Type"][...].squeeze() 
-                SN_KICK = fdata['SSE_Supernovae']["Applied_Kick_Magnitude"][...].squeeze()
+                if mode == 'sse':
+                    key = 'SSE_Supernovae'
+                    suffix = ''
+                if mode == 'bse':
+                    key = 'BSE_Supernovae'
+                    suffix = '(SN)'
+                    
+                SN_STELLAR_TYPE = fdata[key][f"Stellar_Type{suffix}"][...].squeeze()
+                SN_TYPE = fdata[key][f"SN_Type{suffix}"][...].squeeze() 
+                SN_KICK = fdata[key][f"Applied_Kick_Magnitude{suffix}"][...].squeeze()
 
                 maskSN_NS = ((SN_STELLAR_TYPE ==13) * (SN_TYPE == 1)) # select NSs, ignore electron capture SN
                 maskSN_BH = ((SN_STELLAR_TYPE ==14) * (SN_TYPE == 1)) # select BHs, ignore electron capture SN
@@ -41,46 +48,46 @@ def load_sim_data(bh_kicks=[200], ns_kicks=[300, 400, 700], sigmas = [0.1, 0.3, 
     return SN_KICK_NS_ALL, SN_KICK_BH_ALL, NS_KICK_MULT, SIGMAS
 
 
-def load_local_sim_data(bh_kicks=[200], ns_kicks=[400], sigmas = [0.3]):  
-    paths = []
+# def load_local_sim_data(bh_kicks=[200], ns_kicks=[400], sigmas = [0.3]):  
+#     paths = []
     
-    for bh_kick in bh_kicks:
-        for ns_kick in ns_kicks:
-            for sigma in sigmas:
-                path = f'../COMPAS_runs/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
-                paths.append(path)
+#     for bh_kick in bh_kicks:
+#         for ns_kick in ns_kicks:
+#             for sigma in sigmas:
+#                 path = f'../COMPAS_runs/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
+#                 paths.append(path)
             
-    SN_KICK_BH_ALL = []
-    SN_KICK_NS_ALL = []
-    NS_KICK_MULT = []
-    SIGMAS = []
+#     SN_KICK_BH_ALL = []
+#     SN_KICK_NS_ALL = []
+#     NS_KICK_MULT = []
+#     SIGMAS = []
        
     
-    for ns_kick in ns_kicks:
-        for sigma in sigmas:
-            path = f'../COMPAS_runs/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
-            print("Loading Mandel Muller model data from", path)
+#     for ns_kick in ns_kicks:
+#         for sigma in sigmas:
+#             path = f'../COMPAS_runs/bh_{bh_kick}_ns_{ns_kick}_sigma_{sigma}_combined.h5'
+#             print("Loading Mandel Muller model data from", path)
             
-            fdata = h5.File(path, 'r')
+#             fdata = h5.File(path, 'r')
             
-            SN_STELLAR_TYPE = fdata['SSE_Supernovae']["Stellar_Type"][...].squeeze()
-            SN_TYPE = fdata['SSE_Supernovae']["SN_Type"][...].squeeze() 
-            SN_KICK = fdata['SSE_Supernovae']["Applied_Kick_Magnitude"][...].squeeze()
+#             SN_STELLAR_TYPE = fdata['SSE_Supernovae']["Stellar_Type"][...].squeeze()
+#             SN_TYPE = fdata['SSE_Supernovae']["SN_Type"][...].squeeze() 
+#             SN_KICK = fdata['SSE_Supernovae']["Applied_Kick_Magnitude"][...].squeeze()
 
-            maskSN_NS = ((SN_STELLAR_TYPE ==13) * (SN_TYPE == 1)) # select NSs, ignore electron capture SN
-            maskSN_BH = ((SN_STELLAR_TYPE ==14) * (SN_TYPE == 1)) # select BHs, ignore electron capture SN
+#             maskSN_NS = ((SN_STELLAR_TYPE ==13) * (SN_TYPE == 1)) # select NSs, ignore electron capture SN
+#             maskSN_BH = ((SN_STELLAR_TYPE ==14) * (SN_TYPE == 1)) # select BHs, ignore electron capture SN
             
-            SN_KICK_NS = SN_KICK[maskSN_NS]
-            SN_KICK_BH = SN_KICK[maskSN_BH] 
+#             SN_KICK_NS = SN_KICK[maskSN_NS]
+#             SN_KICK_BH = SN_KICK[maskSN_BH] 
 
-            fdata.close()
+#             fdata.close()
             
-            SN_KICK_NS_ALL.append(SN_KICK_NS)
-            SN_KICK_BH_ALL.append(SN_KICK_BH)
-            NS_KICK_MULT.append(ns_kick)
-            SIGMAS.append(sigma)
+#             SN_KICK_NS_ALL.append(SN_KICK_NS)
+#             SN_KICK_BH_ALL.append(SN_KICK_BH)
+#             NS_KICK_MULT.append(ns_kick)
+#             SIGMAS.append(sigma)
             
-    return SN_KICK_NS_ALL, SN_KICK_BH_ALL, NS_KICK_MULT, SIGMAS
+#     return SN_KICK_NS_ALL, SN_KICK_BH_ALL, NS_KICK_MULT, SIGMAS
 
 def p_vi_from_model(vt, model):
     '''
@@ -106,17 +113,19 @@ def p_vi_from_model(vt, model):
     return p_vi_M
 
 
-def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigmas=[0.3], local=False):
+def get_pulsar_probability(pulsar_data_dir, model_data_dir='model_velocities', \
+                           bh_kicks=[200], ns_kicks=[400], sigmas=[0.3], \
+                           output_dir = 'calculatedModelLikelihoods'):
     '''
     This is the MAIN function that takes in a range of Muller Mandel prescriptions and calculates the probability of drawing a given set of pulsars from them.
     
     Arguments:
-    pulsar_data_loc:    The folder which contains the pulsar posterior data
+    pulsar_data_dir:    The folder which contains the pulsar posterior data
+    model_data_dir :    Location of simulated model velocity data with 3d and 2d velocities
     bh_kicks:           List of v_bh multipliers used to generate kicks (from the reference model)
     ns_kicks:           List of v_ns multipliers used to generate kicks (from the reference model)
     sigmas:             List of sigmas used to generate kicks (from the reference model)
-    local:              a flag to switch to local versions of data loading functions. Should be replaced with a model_data_loc 
-                        parameter in the future
+    output_dir:         Location to save the calculated likelihoods for each model
                         
     '''
     
@@ -129,7 +138,7 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
         for ns_kick in ns_kicks:
             for sigma in sigmas:
                 fname = f"vns_{ns_kick}_sigma_{sigma}_velocities"
-                path = 'model_velocities/' + fname
+                path = f"{model_data_dir}/{fname}"
                 print("Loading projected model data from", path)  
                 SN_KICKS_NS.append(np.loadtxt(path, unpack=True, usecols=1))
                 NS_KICK_MULT.append(ns_kick)
@@ -138,7 +147,7 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
                 
     # Read in the posteriors
     vt_all = []
-    for file in glob.glob(f'{pulsar_data_loc}/*.bootstraps'):
+    for file in glob.glob(f'{pulsar_data_dir}/*.bootstraps'):
         vt_all.append(np.loadtxt(file, unpack=True, usecols=5))
     print(f"\n Successfully read {len(vt_all)} pulsar data files\n")
 
@@ -170,7 +179,10 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
 
 
         # Save probabilities of drawing each pulsar given the model
-        fname = f"calculatedModelLikelihoods/vns_{NS_KICK_MULT[k]}_sigma_{SIGMAS[k]}"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+        fname = f"{output_dir}/vns_{NS_KICK_MULT[k]}_sigma_{SIGMAS[k]}"
         print(f"Writing pulsar probabilities to file: {fname}")
         np.savetxt(fname, p_di_M)
 
@@ -180,27 +192,27 @@ def get_pulsar_probability(pulsar_data_loc, bh_kicks=[200], ns_kicks=[400], sigm
         
     return likelihoods
 
-def v3d_to_v2d(bh_kicks=[200], ns_kicks=[400], sigmas=[0.3], local=False):
+def v3d_to_v2d(bh_kicks=[200], ns_kicks=[400], sigmas=[0.3], mode='sse',\
+                  work_dir = os.environ['WORK'] + f'/supernova_remnant_production', output_dir='model_velocities'):
+    
     # Read in the model kicks 
-    if local:
-        SN_KICKS_NS, SN_KICKS_BH, NS_KICK_MULT, SIGMAS = load_local_sim_data(bh_kicks=bh_kicks, ns_kicks=ns_kicks, sigmas=sigmas)
-    else:
-        SN_KICKS_NS, SN_KICKS_BH, NS_KICK_MULT, SIGMAS = load_sim_data(bh_kicks=bh_kicks, ns_kicks=ns_kicks, sigmas=sigmas)
+    SN_KICKS_NS, SN_KICKS_BH, NS_KICK_MULT, SIGMAS = load_sim_data(bh_kicks=bh_kicks, ns_kicks=ns_kicks, sigmas=sigmas, mode=mode, work_dir=work_dir)
         
     for k in range(len(SN_KICKS_NS)):       
         model_data_3d = SN_KICKS_NS[k]  
         model_data = get_projected_velocity(model_data_3d)
+        
         # Save the original ns velocities and the velocity projections for future analysis
-        save_velocities(k, model_data_3d, model_data, NS_KICK_MULT, SIGMAS)
+        save_velocities(k, model_data_3d, model_data, NS_KICK_MULT, SIGMAS, output_dir)
     return
     
     
-def save_velocities(k, model_data_3d, model_data, NS_KICK_MULT, SIGMAS):
+def save_velocities(k, model_data_3d, model_data, NS_KICK_MULT, SIGMAS, output_dir):   
     fname = f"vns_{NS_KICK_MULT[k]}_sigma_{SIGMAS[k]}_velocities"
-    if not os.path.exists('model_velocities'):
-        os.makedirs('model_velocities')
-    np.savetxt("model_velocities/"+fname, np.c_[model_data_3d, model_data], header="3D Velocities \t Projected 2D Velocities")
-    print("Succesfully saved 2D projected velocity in", fname)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    np.savetxt(f"{output_dir}/{fname}", np.c_[model_data_3d, model_data], header="3D Velocities \t Projected 2D Velocities")
+    print("Succesfully saved 2D projected velocity in", f"{output_dir}/{fname}")
     return
     
     
